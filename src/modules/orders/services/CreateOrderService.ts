ae112fa6dc0d7048fffc33030af616ac80a7362b
Amaury Euzebio <amaury@a1tecnologia.info>
2020-06-03 18:06:36 -0300

    API Orders

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,74 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found', 400);
+    }
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (findProducts.length !== products.length) {
+      throw new AppError('Products not found');
+    }
+
+    const newProductsWithPriceAndStock = products.map(product => {
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        ...findProducts.reduce(
+          (acc, val) => {
+            if (val.id === product.id) {
+              return {
+                price: val.price,
+                stockQuantity: Number(val.quantity),
+              };
+            }
+            return acc;
+          },
+          { price: 0, stockQuantity: 0 },
+        ),
+      };
+    });
+
+    const updatedQuantities: IProduct[] = [];
+
+    const productsOrder = newProductsWithPriceAndStock.map(product => {
+      if (product.quantity > product.stockQuantity) {
+        throw new AppError('Quantity not available in stock');
+      }
+
+      updatedQuantities.push({
+        id: product.product_id,
+        quantity: product.stockQuantity - product.quantity,
+      });
+
+      return {
+        product_id: product.product_id,
+        quantity: product.quantity,
+        price: product.price,
+      };
+    });
+
+    const orders = await this.ordersRepository.create({
+      customer,
+      products: productsOrder,
+    });
+
+    await this.productsRepository.updateQuantity(updatedQuantities);
+
+    return orders;
   }
 }
 
